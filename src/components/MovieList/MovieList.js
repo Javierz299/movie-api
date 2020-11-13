078728b4b36409e414000a18a67b87cc688636c6
Shaunna Beck <ShaunnaBeck@Shaunnas-MacBook-Pro.local>
2020-11-13 12:11:35 -0500

    pagnation seems to be working.

--- src/components/MovieList/MovieList.js
+++ src/components/MovieList/MovieList.js
@@ -1,24 +1,29 @@
 import React, { useEffect, useState } from 'react'
 import config from '../../config/config'
 
+import Pagnation from '../Pagnation/Pagnation'
+
 import axios from 'axios'
 
 const MovieList = () => {
     const [movies, setMovies] = useState(null)
+    const [page, setPage] = useState(1)
 
-    useEffect(() => {
-        axios.get(`https://api.themoviedb.org/3/movie/popular?api_key=${process.env.REACT_APP_KEY}&language=en-US&page=1&include_adult=false`)
+    useEffect(() => { //https://api.themoviedb.org
+        axios.get(`https://api.themoviedb.org/3/movie/popular?api_key=${process.env.REACT_APP_KEY}&language=en-US&page=${page}&include_adult=false`)
             .then(response => setMovies(response.data))
        
-    }, [])
+    }, [page])
     console.log("movies",movies)
+    console.log('page',page)
     return (
         <div>
             <h2>Popular movies</h2>
+            {movies && <Pagnation page={page} setPage={setPage} totalPages={movies.total_pages}/>}
             {movies !== null ?
             movies.results.map(movie => (
                 //pass to component
-            <div>
+            <div key={movie.id}>
                 <h2>{movie.title}</h2>
 
                 <img src={`${config.IMG_ENDPOINT}/${movie.poster_path}`} 
