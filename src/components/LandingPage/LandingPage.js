1a1577c2f7ebdb057c8f26227f3cf16391bdd3b0
Shaunna Beck <ShaunnaBeck@Shaunnas-MacBook-Pro.local>
2020-11-30 15:20:59 -0500

    added some content to the landing page

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -1,10 +1,21 @@
 import React from 'react'
 import './landing.css'
+import movieDetails from '../Imgs/LandingImgs/movieDetails.png'
+import searchMovies from '../Imgs/LandingImgs/Search.png'
 
 const LandingPage = () => {
     return (
         <div className="testing-container">
-            Landing Page
+            <h2>Welcome</h2>
+            <p>To get started click on the add button. There,
+                you will have access to search through The Movie Database.
+                Movies Watched will be saved to your local storage.
+            </p>
+            <img src={searchMovies} alt="search movies" />
+            <p>
+                Click on the movie posters in your watched list for movie details.
+            </p>
+            <img src={movieDetails} alt="watched list" />
         </div>
     )
 }
